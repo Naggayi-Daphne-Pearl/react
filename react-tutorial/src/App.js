2fd417747efcde136e63c3fcdc461255c6cf2e03
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-30 04:58:01 -0700

    dynamic values

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -2,10 +2,19 @@
 import './App.css';
 
 function App() {
+  const title = 'welcome to my new blog';
+  const likes = 50;
+  const link = "http://www.google.com";
   return (
     <div className="App">
       <div className="content">
-        <h1>App Conent</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ 24 }</p>
+        <p>{ 'hey, i am daphne' }</p>
+        <p>{ Math.random() *10 }</p>
+
+        <a href={ link }>Google site</a>
 
       </div>
     </div>
