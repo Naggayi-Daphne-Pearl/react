2c5b31953aa68869ed5afcc49d7ca85cbb946d3c
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-30 07:38:05 -0700

    outputting lists

--- react-tutorial/src/Home.js
+++ react-tutorial/src/Home.js
@@ -1,21 +1,18 @@
 import { useState} from 'react'; 
 const Home = () => {
-    //let name = 'daphne';
-    // use state makes the component reactive 
-    const [name, setName] = useState('daphne');
-    const [age, setAge] = useState(24);
-    const handleClick = () => {
-       setName('pearl');
-       setAge(12)
-    }
-
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'daphne', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'pearl', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'naggayi', id: 3 }
+    ])
     return ( 
         <div className="home">
-            <h2>Home page</h2>
-            <p>{ name } is {age} years old</p>
-            
-            <button onClick={handleClick}>Click me</button>
+           { blogs.map((blog) => (
+               <div className="blog-preview" key={blog.id}>
+                   <h2>{blog.title}</h2>
+                   <p>Written by {blog.author}</p>
+               </div>
+           ))} 
         </div>
      );
 }
