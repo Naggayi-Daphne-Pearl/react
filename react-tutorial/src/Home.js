97c6dcec4146fec371df7cf7b8ac356af8182d43
Naggayi Daphne <daphnepearl101@gmail.com>
2022-04-30 06:52:31 -0700

    click events

--- react-tutorial/src/Home.js
+++ react-tutorial/src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('hello');
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name);
+    }
+
+
     return ( 
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick= {() =>handleClickAgain('daphne')} >Click me again</button>
         </div>
      );
 }
